3cfb9a2cbabdd805abc798e42f5a7e67f794cac8
Christopher Rock <chrispebble@gmail.com>
2024-06-02 19:46:22 +0000

    Working version with all 10 cases.  Needs to be updated with actual training link and post-completion survey

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Web version of the HMGPT project.
 
 ## Try it out
 * [HM GPT](https://hmgpt-chrispebble.pythonanywhere.com/) (password required)
+* [HM GPT Review](https://hmgpt-chrispebble.pythonanywhere.com/review) (password required)
 
 Currently there are only two cases: a simple uncomplicated URI, and a URI in a patient with type 2 diabetes.
 
@@ -40,45 +41,14 @@ Finish it up
 * You need to see a medical provider today.
 * Do you have any questions?
 
-
-## Create Cases
-Cases can be created by modifying the `patient-beta.yaml` file.  Below is an example case, with comments explaining each variable.
-
-```yaml
----
-id: uri-basic
-#   id is character string (no spaces) that should be unique to each case
-
-prob_wt: 1
-#   The weighting that should be given to this case for the random selection
-#   process.  The probability that a case is chosen is it's weight, divided
-#   by the total of all weights.  For example, if there are three cases and each
-#   has a prob_wt of 1, then each case has a 33.3% chance of being picked.  But
-#   if two cases are both weighted 1, and the last is weighted 8, then that last
-#   one has an 80% chance of being picked, and the other two each have a 10% chance.
-
-intro: This is an adult female.
-#   This line is displayed to the student when they start a new case.
-
-framing: Act as a patient who has a common cold.  I want you to only reply as the patient.
-    When I ask questions, only answer the immediate question.
-    Do not give extra information. Do not write explanations.
-    Answer the questions and wait for my response.
-    Your vital signs are temperature 100.4 fahrenheit, heart rate 88, respiratory rate 14, pulse oximetry 96%.
-    You take birth control and have not had sex since your last period.
-    You have seasonal allergies.
-#   Framing is fed to chatGPT as a 'system' message.  It should set the stage for
-#   the patient encounter, and include any relevent information required for the student
-#   to correctly interact with the patient.
-
-reminders: Remember to act as a patient with a common cold but do not tell me you have a common cold.
-#   Because chatGPT can sometimes forget, the "reminders" are fed in with each interaction
-#   to keep the AI on track.  If you notice the AI has a tendency to forget something you can add
-#   it to the reminders.
-
-goal: This patient had a common cold.
-    The patient should have been given instructions on how to take care of these symptoms at home.
-    The patient should be told to return if they are not improving or if symptoms worsen.
-#   This is how the student will be evaluated at the end of the interaction.
-#   You should tell the AI what the student was expected to do.
-```
+## Case Descriptions:
+1.  Adult Cold, uncomplicated
+2.  Adult Cold, VS outside of range and chills and body aches
+3.  Sore Throat, uncomplicated
+4.  Sore Throat, CENTOR+ should be tested
+5.  Pediatric Cold, uncomplicated
+6.  Pediatric Cold, with headache and HR 112
+7.  UTI, uncomplicated
+8.  UTI, complicated
+9.  Pregnancy test request, uncomplicated
+10. Pregnancy test request, concern for spotting/pain/VS
--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func, or_, exists, desc
+from sqlalchemy import func, or_, desc
 from openai import OpenAI
 import yaml
 import random
@@ -15,6 +15,10 @@ app.config["DEBUG"] = True
 patientdata_filename = "/home/chrispebble/hmgpt/patient-beta.yaml"
 gpt_model = "gpt-3.5-turbo"
 our_temp = 1.0
+
+
+for rule in app.url_map.iter_rules():
+    print(rule)
 
 # -----------------------------------------------------------------------------
 # --- Function definitions
@@ -60,7 +64,9 @@ def get_this_patient(patient_id, patients_filename=patientdata_filename):
     with open(patients_filename, "r") as file:
         patients = list(yaml.safe_load_all(file))
     for p in patients:
-        if p['id'] == patient_id:
+        print(type(p['id']))
+        print(type(patient_id))
+        if int(p['id']) is int(patient_id):
             return p
     return None
 
@@ -117,43 +123,62 @@ class ChatLog(db.Model):
     user_id = db.Column(db.String(254))              # user id (e-mail)
     line_type = db.Column(db.String(100))
     line_content = db.Column(db.String(4096))
+    patient_id = db.Column(db.Integer)
+    stage = db.Column(db.String(10))
 
 
 # --- GET, POST methods --------------------------------------------------------
 @app.route("/", methods=["GET", "POST"])
 def index():
 
-    # Get OpenAI Client
-    client = get_client()
+    if request.method == "GET":
+        # add code here to collect the user email
+        return render_template("index.html")
+
+    if request.form["action"] == "Start":
+        # get the session id, patient_id
+        user_email = request.form["user_email"]
+        user_email = user_email.strip();
+        return render_template("main_page.html",
+                               user_email=user_email)
+
+
+# --- CASE START PAGE --------------------------------------------------------
+@app.get("/case/<int:case_id>")
+def case_get(case_id):
 
     # --- NEW CONVERSATION ---
 
-    if request.method == "GET":
-        # get the next session id by adding  one to the last (max) session id
-        max_sid = db.session.query(func.max(ChatLog.session_id)).scalar()
+    # get the next session id by adding  one to the last (max) session id
+    max_sid = db.session.query(func.max(ChatLog.session_id)).scalar()
+    # curr_sid = max_sid + 1
+
+    # If the database is empty and max_sid is None, start with session_id 1
+    if max_sid is None:
+        curr_sid = 1
+    else:
         curr_sid = max_sid + 1
 
-        # Randomly choose a patient from the default file
-        pt = get_patient()
+    # Load the patient case indicated by case_id
+    pt = get_this_patient(case_id)
 
-        # add the patient framing to the database
-        new_entry = ChatLog(session_id=curr_sid, line_type="framing", line_content=pt['framing'])
-        db.session.add(new_entry)
+    # Show the main page
+    return render_template("case_page.html",
+                            session_id=curr_sid,
+                            patient_id=pt['id'],
+                            case_id=case_id,
+                            patient_intro=pt['intro'],
+                            chatlog=[])
+                            # user_email is passed in the url query string
+                            # pre/post is passed in the url query string
 
-        # add the patient reminders to the database
-        new_entry = ChatLog(session_id=curr_sid, line_type="reminders", line_content=pt['reminders'])
-        db.session.add(new_entry)
 
-        # commit those DB changes
-        db.session.commit()
+# --- CASE CONTINUE PAGE --------------------------------------------------------
+@app.post("/case_continue") #, methods=["POST"])
+def show_case_continue():
 
-        # Show the main page
-        return render_template("main_page.html",
-                                session_id=curr_sid,
-                                patient_id=pt['id'],
-                                patient_intro=pt['intro'],
-                                chatlog=[])
-
+    # Get OpenAI Client
+    client = get_client()
 
     # --- RECEIVED INPUT FOR CONVERSATION ---
 
@@ -162,6 +187,9 @@ def index():
         # get the session id, patient_id
         curr_sid = request.form["session_id"]
         patient_id = request.form["patient_id"]
+        user_email = request.form["user_email"]
+        case_id = request.form["case_id"]
+        stage = request.form["stage"]
 
         # get the patient associated with this id
         pt = get_this_patient(patient_id)
@@ -173,7 +201,12 @@ def index():
         next_line = request.form["user_input"]
 
         # add the new line of conversation to database
-        new_entry = ChatLog(session_id=curr_sid, line_type="user", line_content=next_line)
+        new_entry = ChatLog(user_id=user_email,
+                            session_id=curr_sid,
+                            patient_id=int(patient_id),
+                            stage=stage,
+                            line_type="user",
+                            line_content=next_line)
         db.session.add(new_entry)
 
         # compile entire conversation into a talk_hx
@@ -186,7 +219,12 @@ def index():
         reply = response.choices[0].message.content
 
         # add the ChatGPT reply to database
-        new_entry = ChatLog(session_id=curr_sid, line_type="assistant", line_content=reply)
+        new_entry = ChatLog(user_id=user_email,
+                            session_id=curr_sid,
+                            patient_id=int(patient_id),
+                            stage=stage,
+                            line_type="assistant",
+                            line_content=reply)
         db.session.add(new_entry)
 
         # now commit all DB changes
@@ -199,44 +237,41 @@ def index():
                                         ChatLog.line_type == "assistant")).all()
 
         # render the new page
-        return render_template("main_page.html",
+        return render_template("case_continue.html",
                                 session_id=curr_sid,
+                                user_email=user_email,
                                 patient_id=patient_id,
+                                case_id=case_id,
+                                stage=stage,
                                 patient_intro=pt['intro'],
                                 chatlog=chats)
 
 
-    # --- DONE WITH CONVERSATION ---
-
-    if request.form["action"] == "End Interaction":
-        return redirect(url_for('index'))
-
-
 
 # --- REVIEW PREVIOUS SESSIONS -------------------------------------------------
-@app.route("/review", methods=["GET", "POST"])
+@app.get("/review") #, methods=["GET"])
 def review_sessions():
-    # Query the database for all unique session IDs that have at least one "user" line
-    # todo: this doesn't work...
-    subquery = exists().where(
-        ChatLog.session_id == ChatLog.session_id,
-        ChatLog.line_type == 'user'
-    )
 
-    # Query to find all distinct session_ids that meet the subquery condition
+    # only display user's messages and chatgpt replies
     sessions = (ChatLog.query
-                .with_entities(ChatLog.session_id)
-                .filter(subquery)
-                .distinct()
-                .order_by(desc(ChatLog.session_id))
-                .all())
+                         .with_entities(ChatLog.session_id, ChatLog.user_id, ChatLog.stage, ChatLog.patient_id)
+                         .filter(ChatLog.line_type == "user")
+                         .distinct()
+                         .order_by(desc(ChatLog.session_id))
+                         .all())
 
     return render_template("review_sessions.html", sessions=sessions)
 
-@app.route("/review/<int:session_id>", methods=["GET"])
+
+@app.get("/review/<int:session_id>") #, methods=["GET"])
 def view_session(session_id):
     # get the current conversation
     chatlog = ChatLog.query.filter_by(session_id=session_id).all()
     transcript = get_transcript(chatlog)
-    return render_template("view_session.html", transcript=transcript, session_id=session_id, user_id=chatlog[0].user_id)
+    return render_template("view_session.html",
+                           transcript=transcript,
+                           session_id=session_id,
+                           user_id=chatlog[0].user_id,
+                           stage=chatlog[0].stage,
+                           patient_id=chatlog[0].patient_id)
 
